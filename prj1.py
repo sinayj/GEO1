5a4531aa68a097cad6c274e3ac69d97108545844
SabaJazi <saba.yousefian72@gmail.com>
2020-05-20 00:23:54 -0500

    commit 3

--- prj1.py
+++ prj1.py
@@ -1,11 +1,73 @@
 #print("test hellooooo")
 #print("changes on the local computer 1")
-
+#-------------------------------------------libraries--------------------------------
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 #import descartes
+import pyodbc
 
+#---------------------------------------
+
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:/WSU FY2012 OP.mdb;')
+cursor = conn.cursor()
+
+# ------------------------SQL command--------------------------------------
+#example inputs to test the query:
+a_v_min = "V140"
+a_v_max = "V2399"
+a_e_min = "E140"
+a_e_max = "E2399"
+a_min = 140
+a_max = 2399
+
+sql_command = '''SELECT [FIPS County Code Table].[County Name], [WSU_OP_Diagnosis].[DIAG]  FROM 
+(
+    ([WSU_OP_Diagnosis] INNER JOIN [WSU_OP_Demographics] ON [WSU_OP_Diagnosis].[CNTRL] = [WSU_OP_Demographics].[CNTRL]) 
+        INNER JOIN 
+    [FIPS County Code Table] 
+        ON [WSU_OP_Demographics].[statecounty] = [FIPS County Code Table].[FIPS_Code]
+) 
+WHERE 
+(
+    (
+        ([WSU_OP_Diagnosis].[DIAG]) > ? & ([WSU_OP_Diagnosis].[DIAG]) < ?
+    ) 
+    & 
+    (
+        ([FIPS County Code Table].[State])='Kansas'
+    )
+) 
+OR 
+(
+    (
+        ([WSU_OP_Diagnosis].[DIAG])> ? & ([WSU_OP_Diagnosis].[DIAG])< ?
+    ) 
+    & 
+    (
+        ([FIPS County Code Table].[State])='Kansas'
+    )
+)
+OR
+(
+    (
+        ([WSU_OP_Diagnosis].[DIAG])> ? & ([WSU_OP_Diagnosis].[DIAG])< ?
+    )  
+    & 
+    (
+        ([FIPS County Code Table].[State])='Kansas'
+    )
+)
+ORDER BY [FIPS County Code Table].[County Name]; '''
+
+
+
+#sql_command = '''SELECT [FIPS County Code Table].[County Name] FROM WSU_OP_Diagnosis;'''
+cursor.execute(sql_command, (a_v_min, a_v_max, a_e_min, a_e_max, a_min, a_max))
+
+for row in cursor.fetchall():
+    print(row)
+#-------------------------------
 gdf=gpd.read_file('GU_CountyOrEquivalent.shp')
 df = pd.DataFrame(gdf)
 df_seg=pd.read_csv("seg.csv")
