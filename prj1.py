122eb49353fab6df27f7cda0f7ecb21385ce96a5
Sina Y Jazi <sinayj68@gmail.com>
2020-05-21 22:43:27 -0700

    comment out the access part(actually!!)

--- prj1.py
+++ prj1.py
@@ -58,65 +58,65 @@ button1.grid(row=1, column=0)
 
 #----------------------DB Start-----------------
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:/WSU FY2012 OP.mdb;')
-cursor = conn.cursor()
-
-# ------------------------SQL command--------------------------------------
-#example inputs to test the query:
-a_v_min = "V140"
-a_v_max = "V230"
-a_e_min = "E140"
-a_e_max = "E230"
-a_min = 140
-a_max = 230
-
-sql_command = '''SELECT [FIPS County Code Table].[County Name], [WSU_OP_Diagnosis].[DIAG]  FROM 
-(
-    ([WSU_OP_Diagnosis] INNER JOIN [WSU_OP_Demographics] ON [WSU_OP_Diagnosis].[CNTRL] = [WSU_OP_Demographics].[CNTRL]) 
-        INNER JOIN 
-    [FIPS County Code Table] 
-        ON [WSU_OP_Demographics].[statecounty] = [FIPS County Code Table].[FIPS_Code]
-) 
-WHERE 
-(
-    (
-        ([WSU_OP_Diagnosis].[DIAG]) > ? & ([WSU_OP_Diagnosis].[DIAG]) < ?
-    ) 
-    & 
-    (
-        ([FIPS County Code Table].[State])='Kansas'
-    )
-) 
-OR 
-(
-    (
-        ([WSU_OP_Diagnosis].[DIAG])> ? & ([WSU_OP_Diagnosis].[DIAG])< ?
-    ) 
-    & 
-    (
-        ([FIPS County Code Table].[State])='Kansas'
-    )
-)
-OR
-(
-    (
-        ([WSU_OP_Diagnosis].[DIAG])> ? & ([WSU_OP_Diagnosis].[DIAG])< ?
-    )  
-    & 
-    (
-        ([FIPS County Code Table].[State])='Kansas'
-    )
-)
-ORDER BY [FIPS County Code Table].[County Name]; '''
-
-
-
-#------for the sake of time saving during test running, i comment this part for now:
-
-#cursor.execute(sql_command, (a_v_min, a_v_max, a_e_min, a_e_max, a_min, a_max))
-
-#for row in cursor.fetchall():
-#    print(row)
+# conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:/WSU FY2012 OP.mdb;')
+# cursor = conn.cursor()
+#
+# # ------------------------SQL command--------------------------------------
+# #example inputs to test the query:
+# a_v_min = "V140"
+# a_v_max = "V230"
+# a_e_min = "E140"
+# a_e_max = "E230"
+# a_min = 140
+# a_max = 230
+#
+# sql_command = '''SELECT [FIPS County Code Table].[County Name], [WSU_OP_Diagnosis].[DIAG]  FROM
+# (
+#     ([WSU_OP_Diagnosis] INNER JOIN [WSU_OP_Demographics] ON [WSU_OP_Diagnosis].[CNTRL] = [WSU_OP_Demographics].[CNTRL])
+#         INNER JOIN
+#     [FIPS County Code Table]
+#         ON [WSU_OP_Demographics].[statecounty] = [FIPS County Code Table].[FIPS_Code]
+# )
+# WHERE
+# (
+#     (
+#         ([WSU_OP_Diagnosis].[DIAG]) > ? & ([WSU_OP_Diagnosis].[DIAG]) < ?
+#     )
+#     &
+#     (
+#         ([FIPS County Code Table].[State])='Kansas'
+#     )
+# )
+# OR
+# (
+#     (
+#         ([WSU_OP_Diagnosis].[DIAG])> ? & ([WSU_OP_Diagnosis].[DIAG])< ?
+#     )
+#     &
+#     (
+#         ([FIPS County Code Table].[State])='Kansas'
+#     )
+# )
+# OR
+# (
+#     (
+#         ([WSU_OP_Diagnosis].[DIAG])> ? & ([WSU_OP_Diagnosis].[DIAG])< ?
+#     )
+#     &
+#     (
+#         ([FIPS County Code Table].[State])='Kansas'
+#     )
+# )
+# ORDER BY [FIPS County Code Table].[County Name]; '''
+#
+#
+#
+# #------for the sake of time saving during test running, i comment this part for now:
+#
+# #cursor.execute(sql_command, (a_v_min, a_v_max, a_e_min, a_e_max, a_min, a_max))
+#
+# #for row in cursor.fetchall():
+# #    print(row)
 #-------------------------DB END -------------------------------
 gdf=gpd.read_file('GU_CountyOrEquivalent.shp')
 df = pd.DataFrame(gdf)
